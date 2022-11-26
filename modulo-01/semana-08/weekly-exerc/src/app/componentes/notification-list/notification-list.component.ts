e49aa9479f51a484c5d05d3c492da373cf50993e
matheus <mathespindola12@gmail.com>
2022-11-25 21:51:10 -0300

    Ex3

---
+++ modulo-01/semana-08/weekly-exerc/src/app/componentes/notification-list/notification-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'natp-notification-list',
+  templateUrl: './notification-list.component.html',
+  styleUrls: ['./notification-list.component.scss']
+})
+export class NotificationListComponent {
+
+}
