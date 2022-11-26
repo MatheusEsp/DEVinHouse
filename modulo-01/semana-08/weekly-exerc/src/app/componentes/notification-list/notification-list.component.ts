2703e0c32c5dadba902e1cb6b4d6aae0dfacb032
matheus <mathespindola12@gmail.com>
2022-11-25 22:57:05 -0300

    Ex8

--- modulo-01/semana-08/weekly-exerc/src/app/componentes/notification-list/notification-list.component.ts
+++ modulo-01/semana-08/weekly-exerc/src/app/componentes/notification-list/notification-list.component.ts
@@ -1,3 +1,4 @@
+import { isNgTemplate } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,17 +8,22 @@ import { Component } from '@angular/core';
 })
 export class NotificationListComponent {
   teste = [{
-    titulo: 'teste1',
-    subtitulo: 'teste2',
-    data: 'teste3'
-  },
-  {
-    titulo: 'teste3',
-    subtitulo: 'teste4',
-    data: 'teste5'
+      titulo: 'teste1',
+      subtitulo: 'teste2',
+      data: 'teste3',
+      estado: false
+    },
+    {
+      titulo: 'teste3',
+      subtitulo: 'teste4',
+      data: 'teste5',
+      estado: false
+    }
+  ]
+  trocaEstado(p:string, e:boolean, i:number) {
+    console.log(p,e,i)
+    this.teste[i].estado = true
+    
+    
   }
-]
-  
-
-
 }
