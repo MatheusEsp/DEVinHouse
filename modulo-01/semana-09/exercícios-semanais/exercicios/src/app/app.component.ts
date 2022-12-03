54cf5b0ce77d2a33510889246d688a7fefa8d723
matheus <mathespindola12@gmail.com>
2022-12-03 10:18:28 -0300

    S09Ex06

--- modulo-01/semana-09/exercícios-semanais/exercicios/src/app/app.component.ts
+++ modulo-01/semana-09/exercícios-semanais/exercicios/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exercicios';
+  nome = 'Jon Snow'
+  ator = 'Kit Hello'
+  img = 'https://ovicio.com.br/wp-content/uploads/2022/11/20221101-ovicio-jon-snow-hbo-555x555.jpg'
+  desc = 'Eu nunca assisti GoT'
 }
--- modulo-01/semana-09/exercícios-semanais/exercicios/src/app/componentes/card/card.component.ts
+++ modulo-01/semana-09/exercícios-semanais/exercicios/src/app/componentes/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,12 +7,16 @@ import { Component } from '@angular/core';
 })
 export class CardComponent {
 
-  personagem: any = {
-    nome: 'Jon Snow',
-    imagem: 'https://tm.ibxk.com.br/2022/08/26/26174558850404.jpg?    ims=704x264',
-    nomeAtor: 'Kit Harington',
-    descricao:
-      'Ator britânico. É mais conhecido por interpretar Jon Snow na     série televisiva Game of Thrones da HBO. Meu marido, mas ele não sabe :3',
-  }
-
+  @Input() personagemNome:any = ''
+  @Input() personagemImg:any = ''
+  @Input() personagemAtor: any = ''
+  @Input() personagemDesc: any = ''
+  
+  // personagem: any = {
+  //   nome: 'Jon Snow',
+  //   imagem: 'https://tm.ibxk.com.br/2022/08/26/26174558850404.jpg?    ims=704x264',
+  //   nomeAtor: 'Kit Harington',
+  //   descricao:
+  //     'Ator britânico. É mais conhecido por interpretar Jon Snow na     série televisiva Game of Thrones da HBO. Meu marido, mas ele não sabe :3',
+  // }
 }
