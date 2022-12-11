765ed690a67f3174205ccd3500ad180205655e9c
matheus <mathespindola12@gmail.com>
2022-12-11 12:40:53 -0300

    Ex7

--- modulo-01/semana-10/weekly-exerc/src/app/pages/home/home.component.ts
+++ modulo-01/semana-10/weekly-exerc/src/app/pages/home/home.component.ts
@@ -6,24 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  card = [{
-    titulo: 'Título1',
-    subtitulo: 'subtítulo1',
-    data: '11/12/2022',
-    estado: false
-  },
-  {
-    titulo: 'Título2',
-    subtitulo: 'Subtítulo2',
-    data: '12/10/2010',
-    estado: false
-  }
-]
-trocaEstado(p:string, e:boolean, i:number) {
-  console.log(p,e,i)
-  this.card[i].estado = true
-  
-  
-}
 
 }
